7c9b11fc2dda28977f4eaf125752dc5fb3da696b
pranav-kukreja <145477313+pranav-kukreja@users.noreply.github.com>
2023-09-20 22:20:00 +0530

    Create day1.cpp

---
+++ day1.cpp
@@ -0,0 +1,8 @@
+// link to the question : https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true 
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!"<< '\n';
+    return 0;
+}
