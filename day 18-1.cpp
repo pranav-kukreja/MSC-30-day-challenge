4ca5e0767fd34209e06b2013e29d6b39822c7a57
pranav-kukreja <145477313+pranav-kukreja@users.noreply.github.com>
2023-10-10 17:02:09 +0530

    Create day 18-1.cpp

---
+++ day 18-1.cpp
@@ -0,0 +1,37 @@
+/*
+QUE: BINARY SEARCH
+input an array and a key and tell whether key is there in array or not 
+*/
+//SOLUTION
+#include <iostream>
+using namespace std;
+bool key_in_array(int array[] , int size , int key){
+    for ( int i =0 ; i<size ; i++ ){
+        if( array[i] == key ){
+            return 1;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    int numbers[100];
+    int size;
+    cout<<"enter size: ";
+    cin>>size;
+    cout<<"enter the elements of the array: ";
+    for(int i = 0 ; i < size ; i++){
+        cin>>numbers[i];
+    }
+    int key;
+    cout<<"enter the element to be searched: ";
+    cin>>key;
+    bool apna_answer= key_in_array(numbers , size , key );
+    if(apna_answer==1){
+        cout<<"key found :) "<<endl;
+    }
+    else{
+        cout<<"key not found :( "<<endl; 
+    }
+    return 0;
+}
