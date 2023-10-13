a74336acb484131d7355932f675ac2e1eab07661
pranav-kukreja <145477313+pranav-kukreja@users.noreply.github.com>
2023-10-13 20:08:49 +0530

    Create day 21 -3.cpp

---
+++ day 21 -3.cpp
@@ -0,0 +1,84 @@
+/*
+print a star / diamond shape made up of '*'s along the perimeter , input an odd no from user
+this is the no of stars across the diagonal of this rhombus
+or the no of rows/levels in the pattern 
+for example if n=9
+
+    *
+   * *
+  *   *
+ *     *
+*       *
+ *     *
+  *   *
+   * *
+    *
+    
+for example if n=21
+
+          *
+         * *
+        *   *
+       *     *
+      *       *
+     *         *
+    *           *
+   *             *
+  *               *
+ *                 *
+*                   *
+ *                 *
+  *               *
+   *             *
+    *           *
+     *         *
+      *       *
+       *     *
+        *   *
+         * *
+          *
+
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int number_of_rows;
+    cout<<"enter the odd no of stars you want across the diagoanl of the diamond(rhombus)"<<endl;
+    cout<<"caution: please enter an odd no only:  "<<endl;
+    cin>>number_of_rows;
+    int n1=(number_of_rows/2)+1;
+    int n2=number_of_rows-n1;
+    
+    for(int i=0;i<n1;i++){
+        for(int j=0;j<n1-i-1;j++){
+            cout<<" ";
+        }
+        for(int k=0; k<((2*i)+1); k++ ){
+            if(k==0 || k==(2*i)){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+                
+        }
+        cout<<endl;
+    }
+    for(int i=0 ;i < n2 ; i++){
+        for(int j = 0 ; j<=i ; j++){
+            cout<<" ";
+        }
+        for(int k=0;k<(number_of_rows-2-(2*i)) ; k++){
+            if(k==0|| k==(number_of_rows-3-(2*i))){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
