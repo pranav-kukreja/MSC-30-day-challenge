d9c367249613fd78ff3cd5d11f0dec7f6ccff11d
pranav-kukreja <145477313+pranav-kukreja@users.noreply.github.com>
2023-10-20 19:36:02 +0530

    Create day 28-1.cpp
    Link: https://leetcode.com/problems/binary-search/description/

---
+++ day 28-1.cpp
@@ -0,0 +1,25 @@
+//QUESTION :  link: https://leetcode.com/problems/binary-search/description/
+//SOLUTION:
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+        int mid= (start) +(end-start)/2;
+        while(start<=end){
+            
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;
+            }
+            else {//if(nums[mid]<target)
+                start=mid+1;
+            }
+            mid= (start) +(end-start)/2;
+        }
+    return -1;
+    }
+};
